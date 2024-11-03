5b223f41602487f89e2ee2d570b5653b1f298f6c
CherepovAlex <westler123@yandex.ru>
2024-11-03 23:56:42 +0300

    fix logic

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -8,7 +8,7 @@ public class Even {
         int counterCorreect = 0;
         //запускаем 3 попытки
         for (int i = 0; i < 3; i++) {
-            //запрошиваем число и ответ игрока
+            //запрашиваем число и ответ игрока
             System.out.print("Question: ");
             int number = (int) (Math.random() * 100);
             System.out.println(number);
@@ -16,34 +16,34 @@ public class Even {
             System.out.print("Your answer: ");
             Scanner answerUser = new Scanner(System.in);
             String evenNumber = answerUser.next();
-            System.out.println(evenNumber);
+//            System.out.println(evenNumber);
 
-            boolean checkEven;
             //проверка четности
-            if (number % 2 == 0) {
-                checkEven = true;
-            } else {
-                checkEven = false;
-            }
-            System.out.println("even = " + checkEven);
-            System.out.println("answer = " +evenNumber);
+            boolean b = false;
+            boolean checkEven = (number % 2 == 0) ? (b = true) : b;
+
             //проверка корректности
-            if (evenNumber.equals("yes") && checkEven == true) {
-                System.out.println("Correct!");
-                counterCorreect++;
-            } else if ((evenNumber.equals("yes") && checkEven == false) || (!evenNumber.equals("no"))) {
-                System.out.println("'" + evenNumber + "'" + " is wrong answer ;(. Correct answer was 'no'.");
-                System.out.println("Let's try again, " + Cli.getUserName());
+            if (checkEven == true) {
+                if (evenNumber.equals("yes")) {
+                    System.out.println("Correct!");
+                    counterCorreect++;
+                } else if (evenNumber.equals("no") || (!evenNumber.equals("no"))) {
+                    System.out.println("'" + evenNumber + "'" + " is wrong answer ;(. Correct answer was 'yes'.");
+                    System.out.println("Let's try again, " + Cli.getUserName());
+                }
             }
-            if (evenNumber.equals("no") && checkEven == false) {
-                System.out.println("Correct!");
-                counterCorreect++;
-            } else if (evenNumber.equals("no") && checkEven == true || (!evenNumber.equals("yes"))) {
-                System.out.println("'" + evenNumber + "'" + " is wrong answer ;(. Correct answer was 'yes'.");
-                System.out.println("Let's try again, " + Cli.getUserName());
+            if (checkEven == false) {
+                if (evenNumber.equals("no")) {
+                    System.out.println("Correct!");
+                    counterCorreect++;
+                } else if (evenNumber.equals("yes") || (!evenNumber.equals("yes"))) {
+                    System.out.println("'" + evenNumber + "'" + " is wrong answer ;(. Correct answer was 'no'.");
+                    System.out.println("Let's try again, " + Cli.getUserName());
+                }
             }
+            //вывод поздравления
+
         }
-        //вывод поздравления
         if (counterCorreect == 3) {
             System.out.println("Congratulations,  " + Cli.getUserName() + "!");
         }
